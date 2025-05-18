daf59858ee7642e9743e21a157a19e35193db9b7
Vitaly <onebeattrue@gmail.com>
2025-05-19 01:53:06 +0300

    fix: поправил импорты

--- remote/src/main/java/com/example/e2e4/remote/di/NetworkModule.kt
+++ remote/src/main/java/com/example/e2e4/remote/di/NetworkModule.kt
@@ -11,6 +11,8 @@ import io.ktor.client.HttpClient
 import io.ktor.client.engine.okhttp.OkHttp
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.logging.DEFAULT
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
@@ -45,8 +47,8 @@ internal interface NetworkModule {
                     })
                 }
                 install(Logging) {
-                    logger = io.ktor.client.plugins.logging.Logger.DEFAULT
-                    level = io.ktor.client.plugins.logging.LogLevel.ALL
+                    logger = Logger.DEFAULT
+                    level = LogLevel.ALL
                 }
             }
         }
